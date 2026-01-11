c4002ae076a6dc7b6d67253d9475ec07dc53b28a
Rene <rrr333nnn333@gmail.com>
2026-01-11 17:05:49 -0400

    change: "/sdcard" to "/storage/emulated/0"

--- boot-completed.sh
+++ boot-completed.sh
@@ -80,12 +80,12 @@ fi
 ## Please note that sometimes the path needs to be added twice or above to be effective ##
 ## Besides, all user apps without root access cannot see the hidden path '/sdcard/<hidden_path>' unless you grant it root access ##
 ## First we need to wait until files are accessible in /sdcard ##
-until [ -d "/sdcard/Android/data" ]; do sleep 1; done
+until [ -d "/storage/emulated/0/Android/data" ]; do sleep 1; done
 
 ## Next we need to set the path of /sdcard/ to tell kernel where the actual /sdcard is ##
-${SUSFS_BIN} set_sdcard_root_path /sdcard
+${SUSFS_BIN} set_sdcard_root_path /storage/emulated/0
 ## Next we need to set the path of /sdcard/ to tell kernel where the actual /sdcard/Android/data is ##
-${SUSFS_BIN} set_android_data_root_path /sdcard/Android/data
+${SUSFS_BIN} set_android_data_root_path /storage/emulated/0/Android/data
 
 # Load custom_sus_map.txt
 if [ -f "${PERSISTENT_DIR}/custom_sus_map.txt" ]; then
@@ -115,14 +115,14 @@ if [ -f "${PERSISTENT_DIR}/custom_sus_path_loop.txt" ]; then
 fi
 
 for i in {0..11}; do
-	${SUSFS_BIN} set_sdcard_root_path /sdcard
-	${SUSFS_BIN} set_android_data_root_path /sdcard/Android/data
+	${SUSFS_BIN} set_sdcard_root_path /storage/emulated/0
+	${SUSFS_BIN} set_android_data_root_path /storage/emulated/0/Android/data
 
 	#### Hide the leaking app path like /sdcard/Android/data/<app_package_name> from syscall ####
 	## Now we can add the path ##
 	if [[ $config_hide_sdcard_android_data == 1 ]]; then
-		for i in $(ls /sdcard/Android/data); do
-			${SUSFS_BIN} add_sus_path /sdcard/Android/data/${i}
+		for i in $(ls /storage/emulated/0/Android/data); do
+			${SUSFS_BIN} add_sus_path /storage/emulated/0/Android/data/${i}
 		done
 		# for i in $(ls /sdcard/Android/media); do
 		# 	${SUSFS_BIN} add_sus_path /sdcard/Android/media/${i}
@@ -135,15 +135,15 @@ for i in {0..11}; do
 	#### Hide path like /sdcard/<target_root_dir> from all user app processes without root access ####
 	## Now we can add the path ##
 	if [[ $config_hide_custom_recovery_folders == 1 ]]; then
-		${SUSFS_BIN} add_sus_path /sdcard/Fox
-		${SUSFS_BIN} add_sus_path /sdcard/TWRP
 		${SUSFS_BIN} add_sus_path /storage/emulated/TWRP
+		${SUSFS_BIN} add_sus_path /storage/emulated/0/Fox
+		${SUSFS_BIN} add_sus_path /storage/emulated/0/TWRP
 	fi
 	
 	if [[ $config_hide_rooted_app_folders == 1 ]]; then
-		${SUSFS_BIN} add_sus_path /sdcard/MT2
-		${SUSFS_BIN} add_sus_path /sdcard/OhMyFont
-		${SUSFS_BIN} add_sus_path /sdcard/AppManager
+		${SUSFS_BIN} add_sus_path /storage/emulated/0/MT2
+		${SUSFS_BIN} add_sus_path /storage/emulated/0/OhMyFont
+		${SUSFS_BIN} add_sus_path /storage/emulated/0/AppManager
 	fi
 
 	sleep 5
